78cb6dfa7519723a3534361869d4872d6866bc3f
kakioshe <louis.kakioshe@gmail.com>
2019-04-25 23:21:09 +1000

    try other form of console

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get("/status", function(req, res) {
     if (err) {
       handleError(res, err.message, "Failed to get emotion.");
     } else {
-      console.log("DOCS DATA", docs)
+      console.log("DOCS DATA", docs[0])
       res.status(200).json(docs);
     }
   });
